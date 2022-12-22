17d08a6159125666b9c81f3041e87ce8a8393887
ludmila wagner <lu.wagner91@gmail.com>
2022-12-22 16:47:28 -0300

    comente webconfig

--- src/main/java/com/portfoliowagner/law/WebConfig.java
+++ src/main/java/com/portfoliowagner/law/WebConfig.java
@@ -1,4 +1,4 @@
-package com.portfoliowagner.law;
+/*package com.portfoliowagner.law;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  *
  * @author Usuario
  */
+/*
 @EnableWebMvc
 
 @Configuration
@@ -24,3 +25,4 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
 }
+*/
